8679beae9c2b48702eca083abcfb61e21c5d7f4c
AlexPronin23 <apronin2005@gmail.com>
2026-03-03 13:22:34 +0700

    Lab1 with Pattern

---
+++ lab01/lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BakeryApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblBakeryStyle = new System.Windows.Forms.Label();
+            this.cmbBakeryStyle = new System.Windows.Forms.ComboBox();
+            this.lblBreadType = new System.Windows.Forms.Label();
+            this.cmbBreadType = new System.Windows.Forms.ComboBox();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.pnlLog = new System.Windows.Forms.Panel();
+            this.pnlControls.SuspendLayout();
+            this.pnlLog.SuspendLayout();
+            this.SuspendLayout();
+
+            // lblTitle
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(280, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "🥐 Пекарня Factory Method";
+
+            // lblBakeryStyle
+            this.lblBakeryStyle.AutoSize = true;
+            this.lblBakeryStyle.Location = new System.Drawing.Point(20, 70);
+            this.lblBakeryStyle.Name = "lblBakeryStyle";
+            this.lblBakeryStyle.Size = new System.Drawing.Size(120, 15);
+            this.lblBakeryStyle.TabIndex = 1;
+            this.lblBakeryStyle.Text = "Стиль пекарни:";
+
+            // cmbBakeryStyle
+            this.cmbBakeryStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBakeryStyle.FormattingEnabled = true;
+            this.cmbBakeryStyle.Items.AddRange(new object[] { "French", "American" });
+            this.cmbBakeryStyle.Location = new System.Drawing.Point(150, 67);
+            this.cmbBakeryStyle.Name = "cmbBakeryStyle";
+            this.cmbBakeryStyle.Size = new System.Drawing.Size(150, 23);
+            this.cmbBakeryStyle.TabIndex = 2;
+
+            // lblBreadType
+            this.lblBreadType.AutoSize = true;
+            this.lblBreadType.Location = new System.Drawing.Point(20, 110);
+            this.lblBreadType.Name = "lblBreadType";
+            this.lblBreadType.Size = new System.Drawing.Size(120, 15);
+            this.lblBreadType.TabIndex = 3;
+            this.lblBreadType.Text = "Тип выпечки:";
+
+            // cmbBreadType
+            this.cmbBreadType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBreadType.FormattingEnabled = true;
+            this.cmbBreadType.Items.AddRange(new object[] { "Baguette", "Croissant", "Brioche", "Sourdough" });
+            this.cmbBreadType.Location = new System.Drawing.Point(150, 107);
+            this.cmbBreadType.Name = "cmbBreadType";
+            this.cmbBreadType.Size = new System.Drawing.Size(150, 23);
+            this.cmbBreadType.TabIndex = 4;
+
+            // btnOrder
+            this.btnOrder.BackColor = System.Drawing.Color.FromArgb(255, 193, 7);
+            this.btnOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrder.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnOrder.Location = new System.Drawing.Point(23, 150);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(277, 40);
+            this.btnOrder.TabIndex = 5;
+            this.btnOrder.Text = "🛒 Заказать";
+            this.btnOrder.UseVisualStyleBackColor = false;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+
+            // txtLog
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Font = new System.Drawing.Font("Consolas", 9F);
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.TabIndex = 6;
+
+            // lblResult
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(0, 128, 0);
+            this.lblResult.Location = new System.Drawing.Point(20, 210);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 19);
+            this.lblResult.TabIndex = 7;
+
+            // pnlControls
+            this.pnlControls.Controls.Add(this.lblTitle);
+            this.pnlControls.Controls.Add(this.lblBakeryStyle);
+            this.pnlControls.Controls.Add(this.cmbBakeryStyle);
+            this.pnlControls.Controls.Add(this.lblBreadType);
+            this.pnlControls.Controls.Add(this.cmbBreadType);
+            this.pnlControls.Controls.Add(this.btnOrder);
+            this.pnlControls.Controls.Add(this.lblResult);
+            this.pnlControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlControls.Location = new System.Drawing.Point(0, 0);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Padding = new System.Windows.Forms.Padding(20);
+            this.pnlControls.Size = new System.Drawing.Size(500, 250);
+            this.pnlControls.TabIndex = 8;
+
+            // pnlLog
+            this.pnlLog.Controls.Add(this.txtLog);
+            this.pnlLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlLog.Location = new System.Drawing.Point(0, 250);
+            this.pnlLog.Name = "pnlLog";
+            this.pnlLog.Padding = new System.Windows.Forms.Padding(20, 10, 20, 20);
+            this.pnlLog.Size = new System.Drawing.Size(500, 200);
+            this.pnlLog.TabIndex = 9;
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Controls.Add(this.pnlLog);
+            this.Controls.Add(this.pnlControls);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "🥐 Пекарня - Factory Method Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlControls.ResumeLayout(false);
+            this.pnlControls.PerformLayout();
+            this.pnlLog.ResumeLayout(false);
+            this.pnlLog.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblBakeryStyle;
+        private System.Windows.Forms.ComboBox cmbBakeryStyle;
+        private System.Windows.Forms.Label lblBreadType;
+        private System.Windows.Forms.ComboBox cmbBreadType;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Panel pnlControls;
+        private System.Windows.Forms.Panel pnlLog;
+    }
+}
---
+++ lab01/lab1/lab1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BakeryApp
+{
+    public partial class Form1 : Form
+    {
+        // ==================== 📦 МОДЕЛИ ====================
+
+        // Типы выпечки
+        public enum BreadType
+        {
+            Baguette,
+            Croissant,
+            Brioche,
+            Sourdough
+        }
+
+        // Абстрактный продукт
+        public abstract class Bread
+        {
+            public string Name { get; protected set; }
+            public string Style { get; protected set; }
+
+            public virtual string Bake()
+            {
+                return $"🔥 Выпекаем {Name} ({Style} стиль)...";
+            }
+
+            public virtual string Wrap()
+            {
+                return $"📦 Заворачиваем {Name} в крафт...";
+            }
+
+            public string GetDescription() => $"🥐 {Style} {Name}";
+        }
+
+        // 🇫🇷 Французские стили
+        public class FrenchBaguette : Bread
+        {
+            public FrenchBaguette() { Name = "Baguette"; Style = "French"; }
+        }
+        public class FrenchCroissant : Bread
+        {
+            public FrenchCroissant() { Name = "Croissant"; Style = "French"; }
+            public override string Wrap()
+            {
+                return "📦 Заворачиваем круассан в фирменную бумагу с логотипом...";
+            }
+        }
+        public class FrenchBrioche : Bread
+        {
+            public FrenchBrioche() { Name = "Brioche"; Style = "French"; }
+        }
+        public class FrenchSourdough : Bread
+        {
+            public FrenchSourdough() { Name = "Sourdough"; Style = "French"; }
+        }
+
+        // 🇺 Американские стили
+        public class AmericanBaguette : Bread
+        {
+            public AmericanBaguette() { Name = "Baguette"; Style = "American"; }
+            public override string Bake()
+            {
+                return "🔥 Выпекаем американский багет (более мягкий)...";
+            }
+        }
+        public class AmericanCroissant : Bread
+        {
+            public AmericanCroissant() { Name = "Croissant"; Style = "American"; }
+        }
+        public class AmericanBrioche : Bread
+        {
+            public AmericanBrioche() { Name = "Brioche"; Style = "American"; }
+        }
+        public class AmericanSourdough : Bread
+        {
+            public AmericanSourdough() { Name = "Sourdough"; Style = "American"; }
+        }
+
+
+        // ==================== 🏭 ФАБРИКИ ====================
+
+        // Абстрактная фабрика (Factory Method)
+        public abstract class Bakery
+        {
+            public Bread OrderBread(BreadType type, Action<string> log)
+            {
+                Bread bread = CreateBread(type);
+                log(bread.Bake());
+                log(bread.Wrap());
+                return bread;
+            }
+
+            protected abstract Bread CreateBread(BreadType type);
+        }
+
+        // Французская пекарня
+        public class FrenchBakery : Bakery
+        {
+            protected override Bread CreateBread(BreadType type)
+            {
+                switch (type)
+                {
+                    case BreadType.Baguette:
+                        return new FrenchBaguette();
+                    case BreadType.Croissant:
+                        return new FrenchCroissant();
+                    case BreadType.Brioche:
+                        return new FrenchBrioche();
+                    case BreadType.Sourdough:
+                        return new FrenchSourdough();
+                    default:
+                        throw new ArgumentException($"Неизвестный тип: {type}");
+                }
+            }
+        }
+
+        // Американская пекарня
+        public class AmericanBakery : Bakery
+        {
+            protected override Bread CreateBread(BreadType type)
+            {
+                switch (type)
+                {
+                    case BreadType.Baguette:
+                        return new AmericanBaguette();
+                    case BreadType.Croissant:
+                        return new AmericanCroissant();
+                    case BreadType.Brioche:
+                        return new AmericanBrioche();
+                    case BreadType.Sourdough:
+                        return new AmericanSourdough();
+                    default:
+                        throw new ArgumentException($"Неизвестный тип: {type}");
+                }
+            }
+        }
+
+
+        // ==================== 🎮 UI ЛОГИКА ====================
+
+        private Bakery _currentBakery;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbBakeryStyle.SelectedIndex = 0;
+            cmbBreadType.SelectedIndex = 0;
+            Log("👋 Добро пожаловать в пекарню!\r\nВыберите стиль и тип выпечки.\r\n");
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string style = cmbBakeryStyle.SelectedItem?.ToString();
+                string typeName = cmbBreadType.SelectedItem?.ToString();
+
+                if (string.IsNullOrEmpty(style) || string.IsNullOrEmpty(typeName))
+                {
+                    MessageBox.Show("Выберите все параметры!", "Внимание",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!Enum.TryParse<BreadType>(typeName, out var breadType))
+                {
+                    throw new ArgumentException($"Неверный тип: {typeName}");
+                }
+
+                // Выбираем фабрику
+                if (style == "French")
+                    _currentBakery = new FrenchBakery();
+                else if (style == "American")
+                    _currentBakery = new AmericanBakery();
+                else
+                    throw new ArgumentException($"Неизвестный стиль: {style}");
+
+                Log($"📋 Заказ: {style} {typeName}");
+                Log(new string('-', 40));
+
+                // Выполняем заказ через фабрику
+                Bread bread = _currentBakery.OrderBread(breadType, Log);
+
+                lblResult.Text = $"✅ Готово: {bread.GetDescription()}";
+                Log($"✨ Результат: {bread.GetDescription()}\r\n");
+            }
+            catch (Exception ex)
+            {
+                Log($"❌ Ошибка: {ex.Message}");
+                MessageBox.Show(ex.Message, "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Log(string message)
+        {
+            if (txtLog.InvokeRequired)
+            {
+                txtLog.Invoke(new Action(() => Log(message)));
+                return;
+            }
+            txtLog.AppendText(message + "\r\n");
+            txtLog.ScrollToCaret();
+        }
+    }
+}
